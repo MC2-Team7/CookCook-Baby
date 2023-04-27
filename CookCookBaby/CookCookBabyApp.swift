bba7a7b14d0da0d87fbbbad73eb2e624ca2ea25c
Ung <jjw3435@naver.com>
2023-04-27 17:28:50 +0900

    init

---
+++ CookCookBaby/CookCookBabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookCookBabyApp.swift
+//  CookCookBaby
+//
+//  Created by JaeUngJang on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct CookCookBabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
