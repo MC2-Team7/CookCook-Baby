79a89da84439e9fc2fe4029698b7d70406dba3eb
eemdeeks <prince9088@gmail.com>
2023-05-01 23:40:16 +0900

    feat : slicing function

--- CookCookBaby/Model/Model.swift
+++ CookCookBaby/Model/Model.swift
@@ -11,10 +11,26 @@ class ingredient : Identifiable {
     let name : String
     let imageKey : String
     let soundKey : String
-    init(name: String, imageKey: String, soundKey: String) {
+    let sliceImageKey : SliceImageKey?
+    init(name: String, imageKey: String, soundKey: String, sliceImageKey : SliceImageKey? = nil) {
         self.name = name
         self.imageKey = imageKey
         self.soundKey = soundKey
+        self.sliceImageKey = sliceImageKey
+    }
+}
+extension ingredient {
+    final class SliceImageKey : Identifiable {
+        let key1 : String
+        let key2 : String
+        let key3 : String
+        let key4 : String
+        init(key1: String, key2: String, key3: String, key4: String) {
+            self.key1 = key1
+            self.key2 = key2
+            self.key3 = key3
+            self.key4 = key4
+        }
     }
 }
 
@@ -26,8 +42,9 @@ extension ingredient {
     )
     static let carrot = ingredient(
         name: "당근",
-        imageKey : "carrot",
-        soundKey: "carrotSound"
+        imageKey : "carrot3D",
+        soundKey: "carrotSound",
+        sliceImageKey: SliceImageKey(key1: "carrotSlice1", key2: "carrotSlice1.1", key3: "carrotSlice2", key4: "carrotSlice3")
     )
     static let fish = ingredient(
         name: "생선",
--- CookCookBaby/View/ChoppingView.swift
+++ CookCookBaby/View/ChoppingView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct ChoppingView: View {
     @ObservedObject var viewModel : ChoppingViewModel
-    @State private var draggedOffset = CGSize.zero
-    @State private var accumulatedOffset = CGSize.zero
-    @State private var index = false
+    @State private var draggedOffset = [CGSize.zero,CGSize.zero,CGSize.zero]
+    @State private var accumulatedOffset = [CGSize.zero,CGSize.zero,CGSize.zero]
+    @State private var index : Int = 0
     @State private var ingredientName = ""
     
     var soundSetting = SoundSetting()
@@ -27,9 +27,14 @@ struct ChoppingView: View {
                                 .scaledToFit()
                                 .frame(width: geo.size.width/10)
                                 .onTapGesture {
-                                    index = true
+                                    index = 1
                                     ingredientName = ingredient.imageKey
                                     SoundSetting.instance.playSound(sound: ingredient.soundKey)
+                                    for i in 0...2 {
+                                        draggedOffset[i] = CGSize.zero
+                                        accumulatedOffset[i] = CGSize.zero
+                                    }
+                                    
                                 }
                                 
                         }
@@ -38,15 +43,60 @@ struct ChoppingView: View {
                     
                 }
                 Spacer()
-                if index {
+                switch index {
+                case 1 :
                     Image(ingredientName)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: geo.size.width * 2/3)
-                        .offset(draggedOffset)
+                        //.resizable()
+                        //.scaledToFit()
+                        //.frame(width: geo.size.width * 2/3)
+                        .offset(draggedOffset[0])
                         .gesture(drag)
+                        .onTapGesture {
+                            index = 2
+                        }
+                case 2 :
+                    HStack{
+                        Image(ingredientName + "Slice1.1")
+                            .offset(draggedOffset[0])
+                            .gesture(drag)
+                            .onTapGesture {
+                                index = 3
+                            }
                         
+                        Image(ingredientName + "Slice1")
+                            .offset(draggedOffset[1])
+                            .gesture(drag1)
+                        
+                    }
+                case 3 :
+                    HStack{
+                        Image(ingredientName + "Slice3")
+                            .offset(draggedOffset[0])
+                            .gesture(drag)
+                        Image(ingredientName + "Slice2")
+                            .offset(draggedOffset[2])
+                            .gesture(drag2)
+                        Image(ingredientName + "Slice1")
+                            .offset(draggedOffset[1])
+                            .gesture(drag1)
+                        
+                    }
+                    
+                default:
+                    Image("")
                 }
+                //if index {
+                //    Image(ingredientName)
+                //        .resizable()
+                //        .scaledToFit()
+                //        .frame(width: geo.size.width * 2/3)
+                //        .offset(draggedOffset)
+                //        .gesture(drag)
+                //        .onTapGesture {
+                //
+                //        }
+                //
+                //}
                 Spacer()
             }
         }
@@ -55,12 +105,42 @@ struct ChoppingView: View {
     var drag : some Gesture {
         DragGesture()
             .onChanged{ gesture in
-                draggedOffset = accumulatedOffset + gesture.translation
+                
+                draggedOffset[0] = accumulatedOffset[0] + gesture.translation
+                
+                //draggedOffset = accumulatedOffset + gesture.translation
             }
             .onEnded { gesture in
-                accumulatedOffset = accumulatedOffset + gesture.translation
+                accumulatedOffset[0] = accumulatedOffset[0] + gesture.translation
+                //accumulatedOffset = accumulatedOffset + gesture.translation
             }
         
+    }
+    var drag1 : some Gesture {
+        DragGesture()
+            .onChanged{ gesture in
+                
+                draggedOffset[1] = accumulatedOffset[1] + gesture.translation
+                
+                //draggedOffset = accumulatedOffset + gesture.translation
+            }
+            .onEnded { gesture in
+                accumulatedOffset[1]  = accumulatedOffset[1] + gesture.translation
+                //accumulatedOffset = accumulatedOffset + gesture.translation
+            }
+    }
+    var drag2 : some Gesture {
+        DragGesture()
+            .onChanged{ gesture in
+                
+                draggedOffset[2] = accumulatedOffset[2] + gesture.translation
+                
+                //draggedOffset = accumulatedOffset + gesture.translation
+            }
+            .onEnded { gesture in
+                accumulatedOffset[2] = accumulatedOffset[2] + gesture.translation
+                //accumulatedOffset = accumulatedOffset + gesture.translation
+            }
     }
 }
 extension CGSize {
