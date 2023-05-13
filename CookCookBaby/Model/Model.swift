9475687a45786187a7ee17a7ed2a7fd51bed78b6
Greed <suby0475@gmail.com>
2023-05-13 18:43:09 +0900

    chore : 아이콘 이미지 변경, 알림 종 아이콘 추가 및 애니메이션 구현

--- CookCookBaby/Model/Model.swift
+++ CookCookBaby/Model/Model.swift
@@ -10,15 +10,17 @@ import Foundation
 class ingredient : Identifiable {
     let name : String
     let imageKey : String
+    let imageBar : String
     let soundKey : String
     let sliceImageKey : SliceImageKey?
     let btnImageKey : String
-    init(name: String, imageKey: String, soundKey: String, sliceImageKey : SliceImageKey? = nil, btnImageKey : String) {
+    init(name: String, imageBar: String, imageKey: String, soundKey: String, sliceImageKey : SliceImageKey? = nil, btnImageKey : String) {
         self.name = name
         self.imageKey = imageKey
         self.soundKey = soundKey
         self.sliceImageKey = sliceImageKey
         self.btnImageKey = btnImageKey
+        self.imageBar = imageBar
     }
 }
 extension ingredient {
@@ -39,18 +41,21 @@ extension ingredient {
 extension ingredient {
     static let tomato = ingredient(
         name: "토마토",
+        imageBar : "tomatoBar",
         imageKey: "tomato",
         soundKey: "tomatoSound",
         btnImageKey: ""
     )
     static let scallion = ingredient(
         name: "대파",
+        imageBar : "scallionBar",
         imageKey: "scallion",
         soundKey: "scallionSound",
         btnImageKey: "greenonionButton"
     )
     static let carrot = ingredient(
         name: "당근",
+        imageBar : "carrotBar",
         imageKey : "carrot3D",
         soundKey: "carrotSound",
         sliceImageKey: SliceImageKey(key1: "carrotSlice1", key2: "carrotSlice1.1", key3: "carrotSlice2", key4: "carrotSlice3"),
@@ -58,42 +63,49 @@ extension ingredient {
     )
     static let fish = ingredient(
         name: "생선",
+        imageBar : "fishBar",
         imageKey: "fish",
         soundKey: "fishSound",
         btnImageKey: "fishButton"
     )
     static let mushroom = ingredient(
         name: "버섯",
+        imageBar : "mushroomBar",
         imageKey: "mushroom",
         soundKey: "mushroomSound",
         btnImageKey: "mushroomButton"
     )
     static let potato = ingredient(
         name: "감자",
+        imageBar : "potatoBar",
         imageKey: "potato",
         soundKey: "potatoSound",
         btnImageKey: "potatoButton"
     )
     static let eggplant = ingredient(
         name: "가지",
+        imageBar : "eggplantBar",
         imageKey: "eggplant",
         soundKey: "eggplantSound",
         btnImageKey: "eggplantButton"
     )
     static let onion = ingredient(
         name: "양파",
+        imageBar : "onionBar",
         imageKey: "onion",
         soundKey: "onionSound",
         btnImageKey: "onionButton"
     )
     static let paprika = ingredient(
         name: "파프리카",
+        imageBar : "paprikaBar",
         imageKey: "paprika",
         soundKey: "paprikaSound",
         btnImageKey: "paprikaButton"
     )
     static let meat = ingredient(
         name: "고기",
+        imageBar : "meatBar",
         imageKey: "meat",
         soundKey: "meatSound",
         btnImageKey: "meatButton"
